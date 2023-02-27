2588ea5195c84ffbe30c38c41be3679d3829209d
Adaku <adaajaghaku@gmail.com>
2023-02-27 06:53:41 -0800

    updates

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,19 +1,10 @@
-#ifnde + MAIN_H
-#define MAIN_H
-
-#include <stdio.h>
-#include <unistd.h>
-
-int_putchar(char c);
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
-int _strlen(char *s);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(chat *s);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-
-#endif
+#include "main.h"
+/**
+ * reset_to_98 - "a function that takes a pointer to an int as parameter and updates the value it points to as 98"
+ * @n: input
+ * Return n
+ */
+void reset_to_98(int *n)
+{
+	*n=98;
+}
