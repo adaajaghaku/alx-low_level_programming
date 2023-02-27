eeae98384c7c4bb1757bc3204d856efcfe686df0
Adaku <adaajaghaku@gmail.com>
2023-02-27 02:39:48 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,15 @@
-#include<stdio.h>
-/*
- * main - print a line
- *
+#include <stdio.h>
+
+
+/**
+ * main - print a line "Programming is like building a multilingual puzzle."
  * Return: 0
  */
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	
 	return (0);
 }
